973c3ce95464172bb357ad16002602679fe7e9ae
Svvmy <benidir91@hotmail.fr>
2022-05-19 15:51:23 +0200

    CSS auth 4

--- emergence/src/components/Auth/Authentification.jsx
+++ emergence/src/components/Auth/Authentification.jsx
@@ -15,14 +15,8 @@ export default function Authentification() {
             </header>
         </div>
 
-         <div className='test'>
-            <button> 
-              <img src={google} alt="google" />
-              salut
-          </button>
-          
-          
-         </div> 
+        
+      
 
 
         <div className='div'> 
@@ -31,11 +25,20 @@ export default function Authentification() {
               <h1 className='text'>Bienvenue !</h1>
               <h2 className='text'>Faites émerger vos projets avec Emergence Design</h2>
           </div>
+              
           
-              <div className="logo">
-                <button><img src={google} alt="img"/> S'inscrire avec Google </button>
-              </div>
           <div className='divBtnList'>
+                <button><img src={google} alt="img"/> S'inscrire avec Google </button>
+              {/*<button> 
+                <div className='divbtn'>
+                  <div className='imgdiv'>
+                    <img className='googleTest' src={google} alt="google" />  
+                  </div>
+                  <div className='text1div'>
+                    S'inscrire avec Google
+                  </div>
+                </div>
+              </button>*/}
               <button><img src={apple} alt="img"/> S'inscrire avec Apple </button>
               <button><img src={mail} alt="img"/> S'inscrire avec un e-mail </button>
               <label className='text'> Vous avez déjà un compte ?</label>

d014c6edff8a6a5d0c8767c956ad7b2e1fdcfbfd
Svvmy <benidir91@hotmail.fr>
2022-05-19 16:45:27 +0200

    CSS auth 5

--- emergence/src/components/Auth/Authentification.jsx
+++ emergence/src/components/Auth/Authentification.jsx
@@ -8,16 +8,11 @@ import mail from './logoMail.png'
 export default function Authentification() {
   return (
     <div className='divAuth'>
-
-        <div>
-            <header>
-                
-            </header>
+        <div className="flex">
+          <img className="imgClass" src={google} alt="img"/>
+          <img className="imgClass" src={google} alt="img"/>
+          <img className="imgClass" src={google} alt="img"/>
         </div>
-
-        
-      
-
 
         <div className='div'> 
           
@@ -28,19 +23,30 @@ export default function Authentification() {
               
           
           <div className='divBtnList'>
-                <button><img src={google} alt="img"/> S'inscrire avec Google </button>
               {/*<button> 
                 <div className='divbtn'>
-                  <div className='imgdiv'>
-                    <img className='googleTest' src={google} alt="google" />  
-                  </div>
-                  <div className='text1div'>
-                    S'inscrire avec Google
-                  </div>
+                <div className='imgdiv'>
+                <img className='googleTest' src={google} alt="google" />  
+                </div>
+                <div className='text1div'>
+                S'inscrire avec Google
+                </div>
                 </div>
               </button>*/}
-              <button><img src={apple} alt="img"/> S'inscrire avec Apple </button>
-              <button><img src={mail} alt="img"/> S'inscrire avec un e-mail </button>
+              <button>
+                  <img className="imgClass" src={google} alt="img"/>S'inscrire avec Google 
+              </button>
+              
+              <button>
+                <img className="imgClass" src={apple} alt="img"/> S'inscrire avec Apple 
+              </button>
+              
+              <button>
+                <img className="imgClass" src={mail} alt="img"/> S'inscrire avec un e-mail 
+              </button>
+              
+              
+              
               <label className='text'> Vous avez déjà un compte ?</label>
               <button className='connect'> Se connecter </button>
           </div>
